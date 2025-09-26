a4239210ab29903b95376fcdd9b36539ddc205d1
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-26 16:22:29 -0400

    relocating to public folder

--- src/frame/index.mjs
+++
@@ -1,9 +0,0 @@
-export * as loader from "./loader.mjs";
-export * as network from "./network.mjs";
-export * as context from "./context.mjs";
-export * as parser from "./parser.mjs";
-
-export * as rpc from "../rpc.mjs";
-
-import * as rpc from "../rpc.mjs";
-rpc.set_role("frame");
--- src/frame/rewrite/noscript.mjs
+++
@@ -1,9 +0,0 @@
-import { site_settings } from "../loader.mjs";
-
-export function rewrite_noscript(noscript_element) {
-  if (!site_settings.allow_js)
-    return;
-  let comment = new Comment(noscript_element.innerHTML);
-  noscript_element.innerHTML = "";
-  noscript_element.append(comment);
-}
