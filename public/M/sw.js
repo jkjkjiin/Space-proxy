f84dbbebebd33bf63c6ebd87487cd2d9035d90cf
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-09-30 15:53:44 -0400

    Create sw.js

---
+++ public/M/sw.js
@@ -0,0 +1,16 @@
+importScripts('/M/meteor.codecs.js')
+importScripts('/M/meteor.config.js')
+importScripts('/M/meteor.bundle.js')
+importScripts('/M/meteor.worker.js')
+
+const meteor = new MeteorServiceWorker()
+function handleRequest(event) {
+  if (meteor.shouldRoute(event)) {
+    return meteor.handleFetch(event)
+  }
+
+  return fetch(event.request)
+}
+self.addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event))
+})
