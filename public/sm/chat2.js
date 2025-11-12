a8fa15081d1426c1335dbbc517139dac44456cc8
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-11-12 09:43:13 -0500

    updated firebase version now chat loads faster this will be for all chat js

--- public/sm/chat2.js
+++ public/sm/chat2.js
@@ -1,9 +1,9 @@
 import { auth, db } from "./firebase.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.3.0/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 import {
     ref, push, onChildAdded, onChildRemoved, onChildChanged,
     remove, update, set, get, runTransaction, onValue, off
-} from "https://www.gstatic.com/firebasejs/12.3.0/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-database.js";
 const channelList = document.getElementById("channels");
 const chatLog = document.getElementById("chatLog");
 const mentionNotif = document.getElementById("mentionNotif");
