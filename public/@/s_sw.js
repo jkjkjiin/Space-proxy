bf4e935a850fc59a2e8e270d5206d65842769a31
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-11-14 08:36:12 -0500

    Update s_sw.js

--- public/@/s_sw.js
+++ public/@/s_sw.js
@@ -7,7 +7,7 @@ if (navigator.userAgent.includes('Firefox')) {
   });
 }
 
-importScripts('/scram/scramjet.all.js');
+importScripts('/$/scramjet.all.js');
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
