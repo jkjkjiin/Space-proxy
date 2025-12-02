896a4fd3048a60bcc3db66ae323ef4cfefaaf487
jkjkjiin <168443331+jkjkjiin@users.noreply.github.com>
2025-12-02 10:59:37 -0500

    Update sw.js

--- public/@/sw.js
+++ public/@/sw.js
@@ -5,27 +5,79 @@
  * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
  * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
  */
-if (navigator.userAgent.includes('Firefox')) {
-    Object.defineProperty(globalThis, 'crossOriginIsolated', {
+importScripts("/@/uv.bundle.js");
+importScripts("/@/uv.config.js");
+//importScripts('/$/scramjet.shared.js');
+//importScripts('/$/scramjet.worker.js');
+importScripts('/$/scramjet.all.js');
+importScripts("/@/workerware.js");
+importScripts(__uv$config.sw || "/uv/uv.sw.js");
+const uv = new UVServiceWorker();
+const ww = new WorkerWare({ debug: false });
+const { ScramjetServiceWorker } = $scramjetLoadWorker();
+const sj = new ScramjetServiceWorker();
+(async function () {
+        await sj.loadConfig();
+})();
+//me when Firefox (thanks vk6)
+if (navigator.userAgent.includes("Firefox")) {
+    Object.defineProperty(globalThis, "crossOriginIsolated", {
         value: true,
-        writable: false,
+        writable: true
     });
 }
 
-importScripts('uv.bundle.js');
-importScripts('uv.config.js');
-importScripts(__uv$config.sw || 'uv.sw.js');
+//where we handle our plugins!!!
+self.addEventListener("message", function (event) {
+    console.log(event.data);
+    uv.config.inject = [];
+    //loop over the required data (we don't verify here as types will take care of us :D)
+    event.data.forEach((data) => {
+        if (data.remove) {
+            if (data.type === "page") {
+                const idx = uv.config.inject.indexOf(data.host);
+                uv.config.inject.splice(idx, 1);
+            } else if (data.type === "serviceWorker") {
+                ww.deleteByName(data.name);
+            }
+        } else {
+            if (data.type === "page") {
+                uv.config.inject.push({
+                    host: data.host,
+                    html: data.html,
+                    injectTo: data.injectTo
+                });
+            } else if (data.type === "serviceWorker") {
+                const wwFunction = eval(data.function);
+                ww.use({
+                    function: wwFunction ? wwFunction : new Function(data.function),
+                    name: data.name,
+                    events: data.events
+                });
+            } else {
+                console.error("NO type exists for that. Only serviceWorker & page exist.");
+                return;
+            }
+        }
+    });
+});
 
-const uv = new UVServiceWorker();
-
-async function handleRequest(event) {
-    if (uv.route(event)) {
-        return await uv.fetch(event);
-    }
-    
-    return await fetch(event.request)
-}
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(handleRequest(event));
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        (async () => {
+            const wwRes = await ww.run(event)();
+            if (wwRes.includes(null)) {
+                return;
+            }
+            if (event.request.url.startsWith(location.origin + __uv$config.prefix)) {
+                return await uv.fetch(event);
+            } 
+            else if (sj.route(event)) {
+                return await sj.fetch(event);
+            }
+            else {
+                return await fetch(event.request);
+            }
+        })()
+    );
 });
